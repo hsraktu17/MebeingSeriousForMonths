3a145763da5d6c46b0e7f0bfff8f0f667359c3aa
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2023-12-18 16:55:56 +0530

    why again

--- understandingNode/index.js
+++ understandingNode/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const fs = require('fs')
 const app = express()
 
 app.get('/',(req,res) =>{
@@ -14,6 +14,15 @@ app.get('/contactme',(req,res)=>{
   res.json({
     contactnumber : 8853989997,
     email : "utkarsh172002srivastava@gmail.com"
+  })
+})
+
+app.get('/whatuwant',(req,res)=>{
+  fs.readFile('a.txt','utf-8',(err,data)=>{
+    if(data){
+      res.status(200)
+      res.end(data)
+    }
   })
 })
 
