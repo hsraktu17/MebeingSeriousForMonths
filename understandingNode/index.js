e5334c5b8d525bcd7709655532ea081523e2be7c
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2023-12-18 16:35:55 +0530

    some changes made

--- understandingNode/index.js
+++ understandingNode/index.js
@@ -6,6 +6,17 @@ app.get('/',(req,res) =>{
   res.send("heeeelo!!")
 })
 
+app.get('/aboutme',(req,res) =>{
+  res.end("Hi, there My name is Utkarsh")
+})
+
+app.get('/contactme',(req,res)=>{
+  res.json({
+    contactnumber : 8853989997,
+    email : "utkarsh172002srivastava@gmail.com"
+  })
+})
+
 app.listen(3000,()=>{
   console.log("Server started!!")
 })
