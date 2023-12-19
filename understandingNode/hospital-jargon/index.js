d15e31bf1a63cb93e1f04eefb0175a092c58608a
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2023-12-19 14:58:10 +0530

    starting today 19/12/23

--- understandingNode/hospital-jargon/index.js
+++ understandingNode/hospital-jargon/index.js
@@ -3,6 +3,8 @@ const app = express()
 const fs = require('fs')
 
 
+app.use(express.json()) // middleware which I dont know what it is.....
+
 const users = [{
   name : 'John',
   kidney : [{
@@ -30,11 +32,13 @@ const users = [{
 ]
 
 
+let totalKidneys = users[0].kidney.length;
 
+//get request is used when user wants the data from the database or from the server
 app.get('/',(req,res) =>{
   const username = users[0].name
-  const johnkidneys = users[1].kidney;
-  const totalkidneys = johnkidneys.length;
+  const johnkidneys = users[0].kidney;
+  // const totalkidneys = johnkidneys.length;
   let healthykidneys = 0;
   
   for(let i = 0; i < johnkidneys.length; i++){
@@ -42,22 +46,51 @@ app.get('/',(req,res) =>{
       healthykidneys += 1;
     }
   }
-  const unhealthykidneys = totalkidneys - healthykidneys
+  const unhealthykidneys = totalKidneys - healthykidneys
   res.json({
     username,
-    totalkidneys,
+    totalKidneys,
     healthykidneys,
     unhealthykidneys
   })
   
 })
 
-app.get('/users',(req,res) =>{
-  res.json(users)
+
+//post request when user wants to post there data in the database
+app.post('/',(req,res)=>{
+
+  const isHealthy = req.body.isHealthy;
+  users[0].kidney.push({
+    healthy : isHealthy,  
+  })
+  res.json({
+    msg : "Done"
+  })
 })
 
 
+app.put('/',(req,res) =>{
+  for(let i = 0; i < users[0].kidney.length; i++){
+    users[0].kidney[i].healthy = true 
+  }
+  res.json({
+    msg : "updated"
+  })
+})
 
+
+app.delete('/',(req,res)=>{
+//  let totalKidneys = users[0].kidney.length;
+  for(let i = 0; i < users[0].kidney.length; i++){
+    if(users[0].kidney[i].healthy === false){
+      totalKidneys-=1;
+    }
+  }
+  res.json({
+    msg : "Deleted the unhealthy oness"
+  })
+})
 
 app.listen(3001,()=>{
   console.log("Server Started !!")
