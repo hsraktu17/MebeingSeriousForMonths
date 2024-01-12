520383e4c119f84b3e325af0b4e0dcbb58e00868
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-01-12 08:56:57 +0530

    Starting 12 jan

--- HttpServerInAllTheLangWhichIsRequired/CLang/Server.c
+++ HttpServerInAllTheLangWhichIsRequired/CLang/Server.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
+#include "Server.h"
 
-int main(){
 
-  printf("hello, world!\n");
+struct Server server_constructor(int domian, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(void))
+{
+  struct Server server;
   
+  server.domain = domain;
+  server.service = service;
+  server.protocol = protocol;
+  server.interface = interface;
+  server.port = port;
+  server.backlog = backlog;
+
+
+
+
 }
---
+++ HttpServerInAllTheLangWhichIsRequired/CLang/Server.h
@@ -0,0 +1,25 @@
+#ifdef Server_h
+#define Server_h
+
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+struct Server
+{
+  int domain;
+  int service;
+  int protocol;
+  u_long interface;
+  int port;
+  int backlog;
+
+
+  struct sockaddr_in address;
+  void(*launch)(void);
+}
+
+
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(void));
+
+
+#endif // DEBUG
