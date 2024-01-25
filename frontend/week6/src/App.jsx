1ac9001c821dd3060d000326b9af3a7cdc55f541
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-01-26 01:21:40 +0530

    keys added

--- frontend/week6/src/App.jsx
+++ frontend/week6/src/App.jsx
@@ -1,12 +1,47 @@
 import { useState } from "react"
 
+let counter = 4;
 function App() {
+    const [todos, setTodos] = useState([{
+      id : 1,
+      title : "go to gym",
+      description : "come to gym"
+    },{
+      id : 2,
+      title : "go to shop",
+      description : "come to shop"
+    },{
+      id : 3,
+      title : "go to market",
+      description : "come to market"
+    }
+  ])
+
+  function addTodo(){
+    setTodos([...todos,{
+      id : counter++,
+      title : "go to class",
+      description : "come to class"
+    }])
+  }
 
   return (
     <div>
-      Hi there;
-      <HeaderWithHeader />
-      <HumanHeader title = "Heeelo" description = "HJKsvkh" />
+      {todos.map(todo => <Todo key = {todo.id} title = {todo.title} description={todo.description} /> )}
+      <button onClick={addTodo}>Add todo</button>
+    </div>
+  )
+}
+
+function Todo({title, description}){
+  return (
+    <div>
+      <h1>
+        {title}
+      </h1>
+      <h5>
+        {description}
+      </h5>
     </div>
   )
 }
