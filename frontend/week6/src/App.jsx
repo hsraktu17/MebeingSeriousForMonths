a77d281a72ef0a515244c8de9413074985519dea
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-01-26 13:26:48 +0530

    wrapper done

--- frontend/week6/src/App.jsx
+++ frontend/week6/src/App.jsx
@@ -1,74 +1,37 @@
 import { useState } from "react"
 
-let counter = 4;
 function App() {
-    const [todos, setTodos] = useState([{
-      id : 1,
-      title : "go to gym",
-      description : "come to gym"
-    },{
-      id : 2,
-      title : "go to shop",
-      description : "come to shop"
-    },{
-      id : 3,
-      title : "go to market",
-      description : "come to market"
-    }
-  ])
-
-  function addTodo(){
-    setTodos([...todos,{
-      id : counter++,
-      title : "go to class",
-      description : "come to class"
-    }])
-  }
-
   return (
     <div>
-      {todos.map(todo => <Todo key = {todo.id} title = {todo.title} description={todo.description} /> )}
-      <button onClick={addTodo}>Add todo</button>
+      <CardWrapper innerComponent = {<TextComponent/>}/>
+      <CardWrapper innerComponent = {<TextComponent/>}/>
+      <Card>
+        <h1>
+          Hi there!!
+        </h1>
+        <h3>
+          I am utkarsh
+        </h3>
+      </Card>
     </div>
   )
 }
 
-function Todo({title, description}){
-  return (
-    <div>
-      <h1>
-        {title}
-      </h1>
-      <h5>
-        {description}
-      </h5>
-    </div>
-  )
+function TextComponent(){
+  return <div>
+    hi there
+  </div>
 }
 
-
-function HeaderWithHeader(){
-  const [title, setTitle] = useState("Hi i am Utkarsh");
-
-  function handleChange(){
-    setTitle("Hi i am "+ Math.random())
-  }
-
-  return (
-    <div>
-      <button onClick = {handleChange}>Handle change</button>
-      <HumanHeader title = {title} description = "Hey aisdfn" />
-    </div>
-  )
+function CardWrapper({innerComponent}){
+  return <div style = {{border : "2px solid black"}}>
+    {innerComponent}
+  </div>
 }
-
-function HumanHeader(props){
-  return(
-    <div>
-      <h1>{props.title}</h1>
-      <h2>{props.description}</h2>
-    </div>
-  )
+function Card({children}){
+  return <div style = {{border : "10px solid green", padding : 100}}>
+    {children}
+  </div>
 }
 
 
