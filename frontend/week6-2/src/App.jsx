9223408735cfa5cc5f62fa3c9781a763de24ce7e
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-01-28 13:42:56 +0530

    useEffect and useState in same time

--- frontend/week6-2/src/App.jsx
+++ frontend/week6-2/src/App.jsx
@@ -1,29 +1,26 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
 
 function App(){
-  return <div>
-    <Todo id = {1}/>
-  </div>
-}
-
-function Todo({id}){
   const [todos, setTodos] = useState([])
 
   useEffect(()=>{
-    axios.get('https://sum-server.100xdevs.com/todo?id'+id)
-    .then(response =>{
-      setTodos(response.data.todos)
-    })
+    fetch('https://sum-server.100xdevs.com/todos')
+      .then(async function(res){
+        const json = await res.json();
+        setTodos(json.todos)
+      })
   },[])
+
   return <div>
-    <h1>
-      {todos.title}
-    </h1>
-    <h3>
-      {todos.description}
-    </h3>
+    {todos.map(todo => <Todo key = {todo.id} title = {todo.title} description={todo.description} />)}
   </div>
 }
 
-export default App
+function Todo({title, description}){
+  return<div>
+    <h1>{title}</h1>
+    <h4>{description}</h4>
+  </div>
+}
+
+export default App;
