c3973f1c6b086cf5dc2645696e8b37ac882cce60
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-02-01 06:05:46 +0530

    typescript types done

--- typescript/week9-2/dist/index.js
+++ typescript/week9-2/dist/index.js
@@ -1,5 +1,26 @@
 "use strict";
-function sum(a, b) {
-    return a + b;
+function isLegal(user) {
+    if (user.age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-console.log(sum(10, 12));
+function greet(user) {
+    console.log("hi " + user.firstName + user.lastName);
+}
+function checkEmail(user) {
+    return !!user.email;
+}
+const user = {
+    firstName: "Utkarsh ",
+    lastName: "Srivastava",
+    age: 21,
+};
+const legal = isLegal(user);
+console.log(legal);
+const greeting = greet(user);
+console.log(greeting);
+const checking = checkEmail(user);
+console.log(checking);
--- typescript/week9-2/src/index.ts
+++ typescript/week9-2/src/index.ts
@@ -1,5 +1,41 @@
-function sum (a : number, b: number):number{
-    return a+b
+interface User{
+    firstName : string
+    lastName : string
+    age : number
+    email ?:string
 }
 
-console.log(sum(10,12))
+function isLegal(user : User) : boolean{
+    if(user.age > 18){
+        return true
+    }
+    else{
+        return false;
+    }
+}
+
+
+function greet(user : User) : void{
+    console.log("hi " + user.firstName + user.lastName)
+}
+
+function checkEmail(user : User) : boolean{
+    return !!user.email;
+}
+
+
+const user = {
+    firstName : "Utkarsh ",
+    lastName : "Srivastava",
+    age : 21,
+}
+
+
+const legal : boolean = isLegal(user)
+console.log(legal)
+
+const greeting = greet(user)
+console.log(greeting)
+
+const checking : boolean = checkEmail(user)
+console.log(checking)
