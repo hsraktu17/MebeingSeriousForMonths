5d5dcd989794cfbb4e651f21666460092eb4e6d2
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-02-01 20:30:25 +0530

    routing partially done

--- frontend/week7-1/src/App.jsx
+++ frontend/week7-1/src/App.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Landing from "./ components/Landing";
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+const Dashboard = React.lazy(() => import("./ components/Dashboard"))
 
 function App(){
   const [count, setCount] = useState(0)
-  return <div>
+  return (
+    <div>
+      
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path='/Dashboard' element = { <Dashboard /> } />
+          <Route path='/' element = { <Landing /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
 
-  </div>
+function AppBar(){
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div style={{background : "pink"}}>
+          Hi from top bar
+          <button onClick={() => navigate('/')}>Landing Page</button>
+          <button onClick={() => navigate('/Dashboard')}>DashBoard page</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
