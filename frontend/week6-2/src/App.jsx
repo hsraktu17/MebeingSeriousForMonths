2256be9f00ad7274f539f790962e87d4ea5bf896
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-02-15 10:36:08 +0530

    add changes

--- frontend/week6-2/src/App.jsx
+++ frontend/week6-2/src/App.jsx
@@ -1,47 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 function App(){
-
-  const[clicker, setClicker] = useState(1)  
-
-  return <div>
-    <button onClick={()=>{
-      setClicker(1)
-    }} > 1</button>
-
-    <button onClick={()=>{
-      setClicker(2)
-    }} >2</button>
-
-    <button onClick={()=>{
-      setClicker(3)
-    }} >3</button>
-
-    <button onClick={()=>{
-      setClicker(4)
-    }} >4</button>
-
-    <Todo id = {clicker}/>
-
-  </div>
+  return (
+    <div>
+      <Todo id = {2}/>
+    </div>
+  )
 }
 
-function Todo ({id}){
-  const [todo, setTodo] = useState({})
+function Todo({id}){
+
+  const [todo, setTodo] = useState([])
 
   useEffect(()=>{
-    fetch('https://sum-server.100xdevs.com/todo?id='+id)
-      .then(async function(res){
-        const json = await res.json()
-        setTodo(json.todo)
+    fetch('https://sum-server.100xdevs.com/todo?id'+'id')
+      .then(async function (res){
+        const json = await res.json();
+        setTodo(json);
       })
   },[id])
 
-  return <div>
-    <p>{id}</p>
-    <h1>{todo.title}</h1>
-    <h4>{todo.description}</h4>
-  </div>
+  return (
+    <div>
+      <h1> {todo.title} </h1>
+      <h4> {todo.description} </h4>
+
+    </div>
+  )
 }
 
 export default App;
